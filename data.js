bdde3ee13f192562aea843c11aba819e22821c11
ElinMolander <105853122+ElinMolander@users.noreply.github.com>
2023-01-06 13:25:07 +0100

    added all but images

---
+++ data.js
@@ -0,0 +1,77 @@
+export const charactersData = [
+    {
+        keywords: ["elephant", "elephants", "animal"],
+        headImage: "elephant-head.png",
+        bodyImage: "elephant-body.png",
+        feetImage: "elephant-feet.png",
+        alt: "part of a Elephant",
+        robotCommentIfMatch: "It´s a elephant, Elephant!!! wow so cute Elephant  ",
+        
+      
+    },
+    { 
+        keywords: ["gorilla", "gorillas","animal"],
+        headImage: "gorilla-head.png",
+        bodyImage: "gorilla-body.png",
+        feetImage: "gorilla-feet.png",
+        alt: "part of a Gorilla",
+        robotCommentIfMatch: "yes! more Gorillas to the humans, you know your 98% a gorilla",
+       
+      
+    },
+    {
+        keywords: ["fish","animal"],
+        headImage: "fish-head.png",
+        bodyImage: "fish-body.png",
+        feetImage: "fish-feet.png",
+        alt: "part of a fish",
+        robotCommentIfMatch: "that was not so hard? A Fish drawing on you comand",
+        
+    },
+    {
+        keywords: ["carrot","veggie"],
+        headImage: "carrot-head.png",
+        bodyImage: "carrot-body.png",
+        feetImage: "carrot-feet.png",
+        alt: "part of a carrot",
+        robotCommentIfMatch: "yummy yumm, wish I could eat some too! ",
+        
+    },
+    {
+        keywords: ["sheep","animal"],
+        headImage: "sheep-head.png",
+        bodyImage: "sheep-body.png",
+        feetImage: "sheep-feet.png",
+        alt: "part of a sheep",
+        robotCommentIfMatch: "finnaly a smart wich, sheep is bassicly like robots",
+        
+    },
+]
+
+export const robotComment = [
+  
+ "I´m not amused by your stupied wish, this masterpeace is milion times better", "that was stupid, this doodle is better",
+  "no human has tuetched this masterpeas, so they say"]
+
+//   "error error, that is to weard to draw, this is is better",
+//   "human you got this anti wish drawing", "just changed some small things, you like it better",
+//  "blähh I have a funny aftertaste from your wish.", "I´m more inspierd by this amizing creatuer",
+//  "You are my musa and made me do this",
+//   "I think this is a better Idé","Dobide doo, You say tomato I say potato", 
+//   "I dont know what this is but its a milion times better than you could ever Wish for",
+
+export const backgroundData = [
+    {
+        keywords: ["water", "sea", "hav"],
+        backgroundImage: "sea.png"
+    },
+    {
+        keywords: ["is", "antartis", "nordpolen"],
+        backgroundImage: "antarktis.png"
+    },
+    {
+        keywords: ["space", "dark", "cold"],
+        backgroundImage: "space.png"
+    },
+]
+
---
+++ index.js
@@ -0,0 +1,400 @@
+
+import { backgroundData,charactersData, robotComment } from '/data.js'
+
+
+
+const robotFace = document.getElementById("eyes-background-wraper")
+const startPopup = document.getElementById("start-popup")
+const speechBubbleText = document.getElementById("ai-text")
+const startBtn = document.getElementById("start-Btn")
+const drawBtn = document.getElementById("draw-Btn")
+const aiTextContainer = document.getElementById("ai-text-container")
+const aiText = document.getElementById("ai-text")
+const canvasContainer = document.getElementById("canvas-container")
+const drawInput = document.getElementById("draw-Input")
+const textline2 = document.getElementById("ai-text-line2")
+let canvasMask = ""
+let canvas = ""
+let printed = false
+
+
+var matchingSertsh = false
+
+
+let aText = new Array("Welcome I'm art-bot","What do you whant me to draw?","Your wish is my comand")
+let iIndex = 0 
+let iArrLength = aText[0].length
+let iSpeed = 50
+let iScrollAt = 20 
+let iTextPos = 0 
+let sContents = ''
+let iRow
+// speechBubbleText.innerHTML=``
+
+
+// ADDEVENTLISTENERS
+
+
+
+window.onload = function(){
+    drawInput.focus()
+    const textToDisplay = ["Welcome I'm art-bot","What do you whant me to draw?","Your wish is my comand"]
+    activateText(textToDisplay)
+    
+
+   
+}
+
+
+
+function activateText(textToDisplay){
+    // speechBubbleText.innerHTML = aText
+    aText = textToDisplay
+    iIndex = 0 
+    iArrLength = aText[0].length
+    iSpeed = 50
+    iScrollAt = 20 
+    iTextPos = 0 
+    sContents = ''
+    iRow
+    typeWriter()
+    
+}
+
+
+
+
+function typeWriter(){
+    sContents= ' '
+    iRow = Math.max(0, iIndex-iScrollAt)
+   
+    while (iRow < iIndex){
+        sContents += aText[iRow++] + '<br />'
+    }
+    speechBubbleText.innerHTML = sContents + aText[iIndex].substring(0,iTextPos)+ "_"
+        if (iTextPos++ === iArrLength){
+            iTextPos = 0
+            iIndex++
+            if(iIndex != aText.length){
+                iArrLength = aText[iIndex].length
+                setTimeout(typeWriter,500)
+            }
+        }else{
+            setTimeout(typeWriter,iSpeed)
+        }
+ }
+
+
+// startBtn.addEventListener("click", function(){
+//     startPopup.style.transition = "opacity 1s ease-out"
+//     startPopup.style.opacity = "0"
+//     eyesOpen()
+//     setTimeout(() => {
+//         startPopup.style.display= "none"
+//         eyesClose()
+//         aiTextContainer.style.display = "block"
+//         aiTextContainer.style.transition = "opacity 1s ease-out"
+//         aiTextContainer.style.opacity = "1"
+//         // aiText.innerHTML = "hello! I´m whating!!!"
+//         // activateText()
+//         typeWriter();
+//     }, 1500); 
+// })
+
+drawBtn.addEventListener("click", function(){
+    if(!printed){
+        renderCharacter()
+        printPapper()
+        printed = true
+    } else {
+        removePapper()
+        setTimeout(()=>{
+            renderCharacter()
+            printPapper()
+           
+        },1500)
+    }
+})
+
+
+
+
+document.getElementById("input-section").addEventListener("focus", function(e){
+    console.log("focus")
+    eyesOpen()
+}, true) 
+
+document.getElementById("input-section").addEventListener("blur", function(e){
+    console.log("blur")
+    eyesClose()
+}, true) 
+
+
+function getMatchingBackgroundArray(){
+    const valueInputArray = drawInput.value.split(" ")
+    var matchingBackgroundArray =[]
+    for (let i =0; i < valueInputArray.length;i++){
+        const matchingBackground = backgroundData.filter(function(background){
+            return background.keywords.includes(valueInputArray[i])
+        })
+        if(matchingBackground.length > 0){
+            matchingBackgroundArray.push(matchingBackground)
+        }
+    }
+    return matchingBackgroundArray.flat(2)
+}
+
+
+function getMatchingCharactersArray(){
+    const valueInputArray = drawInput.value.split(" ")
+    var matchingCharactersArray = []
+    for (let i = 0; i < valueInputArray.length; i++){
+        const matchingCharacter = charactersData.filter(function(character){
+            return character.keywords.includes(valueInputArray[i])
+        })
+        if(matchingCharacter.length > 0){
+            matchingCharactersArray.push(matchingCharacter)
+        }
+    }
+    return matchingCharactersArray.flat(2)
+}
+
+
+function getSingelCaracter(){
+    const characterArray = getMatchingCharactersArray()
+    
+    if(characterArray.length > 0){
+        matchingSertsh = true
+       return getRandomCharacter(characterArray) 
+    }
+    else {
+        matchingSertsh = false
+       return getRandomCharacter(charactersData)
+    }
+}
+function getSingelBackground(){
+    const backgroundArray = getMatchingBackgroundArray()
+    if(backgroundArray.length > 0){
+        return  getRandomBackground(backgroundArray)
+    }
+    else{
+        return getRandomBackground(backgroundData)
+    }
+}
+
+function getRandomBackground(data){
+    var newBackgroundImage = data.map(getBackgroundImage)
+    
+    function getBackgroundImage(background){
+        var backgroundArray =[background.backgroundImage].join(" ")
+        return backgroundArray
+    }
+
+    const randomBackground ={
+        backgroundImage: getrandomNumber(newBackgroundImage),
+    }
+    return randomBackground
+}
+var counter =  0
+function getRandomCharacter(data){
+   
+    var newHeadImage = data.map(getHeadImage)
+    var newbodyImage = data.map(getbodyImage)
+    var newfeetImage = data.map(getfeetImage)
+    var newrobotCommentIfMatch = data.map(getrobotCommentIfMatch)
+    // var newrobotComment = robotComment
+
+
+    function getHeadImage(head){
+        var headArray = [head.headImage].join(" ")
+        return headArray
+    }
+
+    function getbodyImage(body){
+        var bodyArray = [body.bodyImage].join(" ")
+        return bodyArray
+    }
+
+    function getfeetImage(feet){
+        var feetArray = [feet.feetImage].join(" ")
+        return feetArray
+    }
+
+    function getrobotCommentIfMatch(coment){
+        var comentArrayIfMatch = [coment.robotCommentIfMatch].join(" ")
+        return comentArrayIfMatch
+    }
+   
+   
+    const randomcharacter = {
+        headImage: getrandomNumber(newHeadImage),
+        bodyImage: getrandomNumber(newbodyImage),
+        feetImage: getrandomNumber(newfeetImage),
+        robotCommentIfMatch: getrandomNumber(newrobotCommentIfMatch),
+        
+    }
+   return randomcharacter 
+}
+
+function getNextIndexNummber(data){
+  
+    counter += 1
+    console.log(`innan if ${counter}`)
+    if ( counter >= data.length){
+        counter = 0
+    } 
+   return data[counter]
+
+}
+
+function getrandomNumber(data){
+    const randomNumber = Math.floor (Math.random()* data.length)
+    return data[randomNumber]
+    
+}
+
+function renderAiText(data){
+    var newrobotComment = robotComment
+    var aiComentObject = getSingelCaracter()
+    var robotCommentText = getNextIndexNummber(newrobotComment)
+   
+    const valueInputArray = drawInput.value
+    
+    setTimeout(()=>{
+        aiTextContainer.style.display = "block"
+        aiTextContainer.style.transition = "opacity 1s ease-out"
+        aiTextContainer.style.opacity = "1"
+    },500)
+if (matchingSertsh){
+    setTimeout(()=>{
+        // aiText.classList.add("ai-text-animation")
+        const textToDisplay = [`You wanted me to draw ${valueInputArray},
+        and here it is ${aiComentObject.robotCommentIfMatch}`]
+        activateText(textToDisplay)
+        matchingSertsh = false
+
+    },500)
+} else{
+    setTimeout(()=>{
+        // aiText.classList.add("ai-text-animation")
+        const textToDisplay = [`You wanted me to draw ${valueInputArray},
+        ${robotCommentText}`]
+        activateText(textToDisplay)
+    },500)
+
+}
+    
+   
+
+   
+   
+}
+
+function renderCharacter(){
+    const characterObject = getSingelCaracter()
+    const backgroundObject = getSingelBackground()
+    renderAiText()
+    console.log(backgroundObject)
+
+    var divMask = document.createElement("div")
+    divMask.id = "canvas-mask"
+    divMask.className = "canvas-mask"
+
+    var divCanvas = document.createElement("div")
+    divCanvas.id = "canvas"
+    divCanvas.className ="canvas"
+
+    divMask.appendChild(divCanvas)
+
+    canvasContainer.appendChild(divMask)
+    canvas = divCanvas
+    canvasMask = divMask
+    divCanvas.innerHTML = `
+        <div class="canvas-color"> </div>
+        <img class="renderd-img" alt="${characterObject.alt}" src="/images/drawing-components/${characterObject.headImage}">
+        <img class="renderd-img body" alt="${characterObject.alt}" src="/images/drawing-components/${characterObject.bodyImage}">
+        <img class="renderd-img" alt="${characterObject.alt}" src="/images/drawing-components/${characterObject.feetImage}">
+        <img class="renderd-background-img" alt="${characterObject.alt}" src="/images/drawing-components/${backgroundObject.backgroundImage}">
+        <img class="canvas-img" id="canvas-img" src="/images/canvas-noborder.png">
+        `
+    canvas.style.transform = "translateX(-50%) translateY(-100%)"
+}    
+    
+
+function printPapper(){
+    mouthOpen()
+    setTimeout(()=>{
+        canvas.style.transition = "transform 2s ease-out"
+        canvas.style.transform = "translateX(-50%) translateY(15%)"
+    },1000)
+}
+
+function removePapper(){
+    canvasMask.style.transition = " transform 1s ease-out"
+    canvasMask.style.transform = "translateX(-50%) translateY(800px) rotate(360deg) scale(.5)"
+    setTimeout(()=>{
+        canvasMask.style.display ="none"
+    },1500)
+}
+
+
+
+
+
+function eyesClose(){
+    robotFace.innerHTML = `
+    <div class="eye-wraper">
+        <img class="eye-img" src="/images/eye-close.png">
+        <img class="eye-img" src="/images/eye-close.png">
+    </div>
+    <img class="galler-img galler-img-eye" src="/images/galler.png">
+    `
+}
+
+function eyesOpen(){
+    robotFace.innerHTML = `
+    <div class="eye-wraper">
+        <img class="eye-img" src="/images/eye.png">
+        <img class="eye-img" src="/images/eye.png">
+    </div>
+    <img class="galler-img galler-img-eye" src="/images/galler.png">
+    `
+}
+
+function mouthOpen(){
+    const mouthUnder = document.getElementById("mouth-under-wraper")
+    const mouthUnderBackground = document.getElementById("mouth-under-background")
+    mouthUnder.style.transition = "top .3s ease-in"
+    mouthUnder.style.top ="60px"
+    mouthUnderBackground.style.transition = "height .3s  ease-in,top .3s  ease-in "
+  
+    mouthUnderBackground.style.height = "5em"
+    mouthUnderBackground.style.top= "-3em"
+
+}
+
+const shareData = {
+    title: "art-bot",
+    text: "Draws arty images from your wishes",
+    url: "https://www.elinmolander.com"
+}
+
+const sharebtn = document.querySelector(".share-btn")
+const resultParagraph = document.querySelector(".result")
+
+sharebtn.addEventListener("click", async()=>{
+    try{
+        await navigator.share(shareData)
+        resultParagraph.textContent = "art-bot shared successfully"
+    } catch (err){
+        resultParagraph.textContent = ` Error: ${err}`
+    }
+})
+
+
+// setTimeout(()=>{
+//     aiEyeHtml.innerHTML = `<img src="/images/Eye.png">
+//     <img src="/images/Eye.png">`
+// },1000)
+
+
