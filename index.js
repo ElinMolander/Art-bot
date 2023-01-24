4a04db7efac551ad0a02f09c0bcc7350b687cbba
ElinMolander <105853122+ElinMolander@users.noreply.github.com>
2023-01-24 10:40:35 +0100

    Updated tracing

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ const mouthUnderBackground = document.getElementById("mouth-under-background")
 const robotGetsTierdOnloadDelay = 30000
 const robotGetsTierdDelay = 40000
 const robotBlinkDelay = 300
-let uuid = self.crypto.randomUUID();
 let canvasMask = ""
 let canvas = ""
 let printed = false
@@ -39,8 +38,6 @@ let typeWriterTimer
 // ONLOAD
 
 window.onload = function(){
-    td = window.td || []
-    td.push(["app", "A1E77D48-E802-4DCC-8515-334FC5107DF6"], ["user", uuid], ["signal"])
     drawInput.focus()
     const textToDisplay = ["Hi, My name is Al, The Art-Bot.", "What do you want me to draw?", "Your wish is my command"]
     activateText(textToDisplay)
